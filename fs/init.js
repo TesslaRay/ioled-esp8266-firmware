cd92a72c448b4f6bdf91804a89a09680774358e1
Eduardo Hales <negro.hales@gmail.com>
2019-03-09 19:56:17 -0300

    added sensors

--- fs/init.js
+++ fs/init.js
@@ -6,13 +6,14 @@ load('api_pwm.js');
 load('api_mqtt.js');
 // Modules.
 load('board.js');
-//load('neopixel.js');
+load('sensors.js');
+load('neopixel.js');
 
 // Topic to send events.
 let eventTopic = '/devices/' + Cfg.get('device.id') + '/events';
 // Topic to receive config.
 let configTopic = '/devices/' + Cfg.get('device.id') + '/config';
-// Topic to send state data. 
+// Topic to send state data.
 let stateTopic = '/devices/' + Cfg.get('device.id') + '/state';
 
 // Initialize all led stored config.
@@ -31,13 +32,19 @@ GPIO.set_mode(board.led5.pin, GPIO.MODE_OUTPUT);
  * period: debounce interval in milliseconds
  * handler: is a function that receives pin number.
  * Return value: 1 in case of success, 0 otherwise.
-*/
-GPIO.set_button_handler(board.btn1.pin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 10, function(button) {
-  ledPwmOff(board[board.btn1.ctrl], true);
-  print('freq : ', board[board.btn1.ctrl].freq);
-  print('duty : ', board[board.btn1.ctrl].duty);
-}, null);
-
+ */
+GPIO.set_button_handler(
+	board.btn1.pin,
+	GPIO.PULL_UP,
+	GPIO.INT_EDGE_NEG,
+	10,
+	function(button) {
+		ledPwmOff(board[board.btn1.ctrl], true);
+		print('freq : ', board[board.btn1.ctrl].freq);
+		print('duty : ', board[board.btn1.ctrl].duty);
+	},
+	null
+);
 
 /* Subscribe to a MQTT topic and receive config data from IoT Core.
  * @configTopic (str): mqtt topic to subscribe.
@@ -51,8 +58,7 @@ GPIO.set_button_handler(board.btn1.pin, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 10, fun
  * --- {"board": {"led1":{"freq":20, "duty": 0.5, "state": true}, "led2":{"freq":20, "duty": 0.5, "state": true}}}
  */
 MQTT.sub(configTopic, function(conn, topic, msg) {
-  print('conn', conn);
-  print('Topic:', topic, 'message:', msg);
-  setBoardConfigV2(msg, applyBoardConfig);
+	print('Topic:', topic, 'message:', msg);
+	setBoardConfigV2(msg, applyBoardConfig);
 });
 
