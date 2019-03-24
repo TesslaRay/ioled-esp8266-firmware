6723a3fc0b93be8ca1543dec684f58e17b542d22
Eduardo Hales <negro.hales@gmail.com>
2019-03-24 01:34:52 -0300

    minor refactor and jsdoc links

--- fs/neopixel.js
+++ fs/neopixel.js
@@ -1,15 +1,12 @@
 load('api_neopixel.js');
 load('api_events.js');
 
-// The pixel index.
-let pixel = 0;
 // Get the neopixel pin (13)
 let pin = Cfg.get('board.neopixel.pin');
 // Get the number of pixels (3).
 let numPixels = Cfg.get('board.neopixel.pixels');
 // Create a and return a neoPixel strip object.
 let strip = NeoPixel.create(pin, numPixels, NeoPixel.GRB);
-
 // RGB colors.
 let red = {r: 200, g: 0, b: 0};
 let green = {r: 0, g: 200, b: 0};
@@ -21,9 +18,12 @@ let timerId;
 /**
  * Network search function.
  * @description Pixel blinks on network discover. Stop blinking when connected.
+ * @see https://github.com/mongoose-os-libs/mjs/blob/master/fs/api_events.js
  */
 let netSearch = function() {
-	// Initialize before using.
+	// The pixel index.
+	let pixel = 0;
+	// Initialize strip.
 	initStrip();
 	// Set timer to change pixel every 500 ms.
 	timerId = Timer.set(
@@ -36,7 +36,7 @@ let netSearch = function() {
 		null
 	);
 
-	// Stop the timer on connection.
+	// Stop the timer on succeed cloud connection.
 	Event.addHandler(
 		Event.CLOUD_CONNECTED,
 		function() {
@@ -50,6 +50,7 @@ let netSearch = function() {
 
 /**
  * Initialize the strip.
+ * @see https://github.com/mongoose-os-libs/neopixel/blob/master/mjs_fs/api_neopixel.js
  */
 let initStrip = function() {
 	strip.clear();
@@ -61,19 +62,19 @@ let initStrip = function() {
 
 /**
  *  Paint only one pixel of the strip.
- * @param {number} pixel The pixel index.
+ * @param {number} index The pixel index.
  * @param {{r: number, g: number, b: number}} color RGB color object.
  */
-let setOnePixel = function(pixel, color) {
+let setOnePixel = function(index, color) {
 	strip.clear();
 	for (let i = 0; i < numPixels; i++) {
-		pixel === i ? strip.setPixel(i, color.r, color.g, color.b) : strip.setPixel(i, 0, 0, 0);
+		index === i ? strip.setPixel(i, color.r, color.g, color.b) : strip.setPixel(i, 0, 0, 0);
 	}
 	strip.show();
 };
 
 /**
- * Paint all pixels on the stirp.
+ * Paint all pixels on the strip.
  * @param {{r: number, g: number, b: number}} color RGB color object.
  */
 let setAllPixels = function(color) {
